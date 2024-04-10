cec2b138a314f9865867c953033b34da67877af9
Raúl Marcano <raulmarcano@MBP-de-Irene.home>
2024-04-10 17:39:05 +0200

    fixed norm

--- src/great_sort.c
+++ src/great_sort.c
@@ -12,46 +12,49 @@
 
 #include "../include/push_swap.h"
 
-void ft_push_remain(t_list **stack_a, t_list **stack_b, int size)
+void	ft_push_remain(t_list **st_a, t_list **st_b, int size, int total_size)
 {
-    while (size > 3)
+	while (size > 3)
 	{
-		if ((*stack_a)->index == size
-			|| (*stack_a)->index == size - 1
-			|| (*stack_a)->index == size - 2)
-			ft_rotate(stack_a, 'a');
+		if ((*st_a)->index == total_size
+			|| (*st_a)->index == total_size - 1
+			|| (*st_a)->index == total_size - 2)
+			ft_rotate(st_a, 'a');
 		else
-        {
-            ft_push(stack_a, stack_b, 'b');
-            size--;
-        }
-    }
+		{
+			ft_push(st_a, st_b, 'b');
+			size--;
+		}
+	}
 }
 
-void ft_push_all(t_list **stack_a, t_list **stack_b)
+void	ft_push_all(t_list **stack_a, t_list **stack_b)
 {
-    int size;
-    int half;
+	int	size;
+	int	total_size;
+	int	half;
 
-    size = ft_lstsize(*stack_a);
-    if(size % 2 == 0)
-        half = size/2;
-    else
-        half = size/2 + 1;
-    while(size >= 3 && size > half)
-    {
-        if(((*stack_a)->index) < half)
-        {
-            ft_push(stack_a, stack_b, 'b');
-            size--;
-        }
-        else
-            ft_rotate(stack_a, 'a');
-    }
-    ft_push_remain(stack_a, stack_b, size);
-    ft_sort_three(stack_a);
+	total_size = ft_lstsize(*stack_a);
+	size = total_size;
+	if (size % 2 == 0)
+		half = size / 2;
+	else
+		half = (size / 2) + 1;
+	while (size >= 3 && size > half)
+	{
+		if (((*stack_a)->index) < half)
+		{
+			ft_push(stack_a, stack_b, 'b');
+			size--;
+		}
+		else
+			ft_rotate(stack_a, 'a');
+	}
+	ft_push_remain(stack_a, stack_b, size, total_size);
+	ft_sort_three(stack_a);
 }
+
 void	ft_great_sort(t_list **stack_a, t_list **stack_b)
 {
-    ft_push_all(stack_a, stack_b);
+	ft_push_all(stack_a, stack_b);
 }
